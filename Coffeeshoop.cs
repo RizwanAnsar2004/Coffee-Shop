d1af229fd2d1249fee3b8717ee14ef515de28b71
RizwanAnsar2004 <Ansarrizwan83@gmail.com>
2024-12-30 07:56:05 +0500

    first commit

---
+++ Coffeeshoop.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ProjectSDA.Models;
+
+namespace ProjectSDA
+{
+    public sealed class AdminLoginManager
+    {
+        private static readonly Lazy<AdminLoginManager> instance = new Lazy<AdminLoginManager>(() => new AdminLoginManager());
+
+        private bool isLoggedIn = false;
+        private string currentAdminUsername;
+
+        private AdminLoginManager() { }
+
+        public static AdminLoginManager Instance
+        {
+            get { return instance.Value; }
+        }
+
+        public bool IsLoggedIn
+        {
+            get { return isLoggedIn; }
+            private set { isLoggedIn = value; }
+        }
+
+        public string CurrentAdminUsername
+        {
+            get { return currentAdminUsername; }
+            private set { currentAdminUsername = value; }
+        }
+
+        public bool Login(Admin_Profile admin)
+        {
+            if (!isLoggedIn)
+            {
+                isLoggedIn = true;
+                currentAdminUsername = admin.Admin_Username;
+                return true;
+            }
+            return false;
+        }
+
+        public void Logout()
+        {
+            isLoggedIn = false;
+            currentAdminUsername = null;
+        }
+    }
+}
